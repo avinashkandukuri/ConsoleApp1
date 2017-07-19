f426b539cc44e9b95e1b66fb643eda27c2d6ef55
avinash kandukuri <avinash kandukuri>
2017-07-19 13:27:07 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String x = "fjdkkdsjfk";
+            Console.Write(x);
+            Console.Write("helloworld");
+            Console.Read();
+
+        }
+    }
+}
